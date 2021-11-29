eef6cb27af2dda2e5882c1029fb119dafc3ed61f
pali7no <pajo7.klein@gmail.com>
2021-11-29 14:06:37 +0100

    Trash
    These files should have been added to .gitignore in the first commit :-1:

--- src/main/java/sk/uniba/fmph/dcs/SimpleDominionInterface.java
+++ src/main/java/sk/uniba/fmph/dcs/SimpleDominionInterface.java
@@ -5,6 +5,6 @@ import java.util.Optional;
 interface SimpleDominionInterface {
     public Optional<GameState> playCard(int handIdx);
     public Optional<GameState> endPlayCardPhase();
-    public Optional<GameState> buyCard(int buyCardIdx);
+    public Optional<GameState> buyCard(int buyDeckIdx, int buyCardIdx);
     public Optional<GameState> endTurn();
 }
